2dc6d1bb5525226ba1825d17346ab2e916605eca
Nathan Lu <nathanlumax@gmail.com>
2022-03-16 15:45:44 -0500

    from the laptop >:)

---
+++ src/contentScript.ts
@@ -0,0 +1,17 @@
+// var listener = function(evt) {
+//     var selection = window.getSelection();
+//     if (selection.rangeCount > 0) {
+//         var range = selection.getRangeAt(0);
+//         var text = range.cloneContents().textContent;
+//         console.log(text);
+//     }
+// };
+// document.addEventListener('dblclick', listener);
+
+// const myUrlWithParams = new URL("https://www.example.dev/");
+//
+// myUrlWithParams.searchParams.append("city", "Rome");
+// myUrlWithParams.searchParams.append("price", "200");
+//
+// console.log(myUrlWithParams.href);
+
