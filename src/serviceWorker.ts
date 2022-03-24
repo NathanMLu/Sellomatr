24c9386f1e4fca1b8212673bf4f5d3536894d7bd
Nathan Lu <nathanlumax@gmail.com>
2022-03-24 17:15:47 -0500

    retrieve keys from server, unencrypt, and create ebayauthtoken using app tokens

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -1,138 +1,97 @@
 import {getStorageItem, setStorageData, setStorageItem} from './storage';
 
+const CryptoJS = require("crypto-js");
+
 /*
  * Helper Functions
  */
 
 function callback() {
     // TODO: create sync function to update the UI (call only when popup is opened or when token is generated)
-    
+
     getOrders();
 }
 
- async function authenticateUser(refreshToken?: string) {
+function decrypt(encrypted: string, iv: string, salt: string): string {
+    iv = CryptoJS.enc.Utf8.parse(iv);
+    salt = CryptoJS.enc.Utf8.parse(salt);
 
-     // Get the token from the server
-     const EbayAuthToken = require('ebay-oauth-nodejs-client');
+    let decrypted = CryptoJS.AES.decrypt(encrypted, salt, {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+    });
+    decrypted = decrypted.toString(CryptoJS.enc.Utf8);
 
-     const fetchKeys = await fetch('https://sellomatr.herokuapp.com/keys', {
-         method: 'POST',
-         headers: {
-             'Content-Type': 'application/json',
-             'Accept': 'application/json'
-         },
-         body: JSON.stringify({ })
-     })
+    return decrypted;
+}
 
-     const fetchIv = await fetch('https://sellomatr.herokuapp.com/iv', {
-         method: 'POST',
-         headers: {
-             'Content-Type': 'application/json',
-             'Accept': 'application/json'
-         },
-         body: JSON.stringify({ })
-     })
+async function authenticateUser(refreshToken?: string) {
 
-     const fetchSalt = await fetch('https://sellomatr.herokuapp.com/salt', {
-         method: 'POST',
-         headers: {
-             'Content-Type': 'application/json',
-             'Accept': 'application/json'
-         },
-         body: JSON.stringify({ })
-     })
+    // Get the token from the server
+    const EbayAuthToken = require('ebay-oauth-nodejs-client');
 
-     const encryptedKeys = await fetchKeys.text();
-     const iv = await fetchIv.text();
-     const salt = await fetchSalt.text();
+    const fetchKeys = await fetch('https://sellomatr.herokuapp.com/keys', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({})
+    })
 
-     //const keys = await JSON.parse(encryptedKeys);
-     const CryptoJS = await require("crypto-js");
+    const fetchIv = await fetch('https://sellomatr.herokuapp.com/iv', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({})
+    })
 
-     // cipher = AES.new(key, AES.MODE_CBC, iv=iv)
-     // original_data = unpad(cipher.decrypt(ciphered_data), AES.block_size)
-     // const encrypted = CryptoJS.AES.encrypt("TestBird", "test");
-     // console.log("Encrypted: ", encrypted.toString());
-     //
-     // const decrypted = CryptoJS.AES.decrypt(encrypted, "test").toString(CryptoJS.enc.Utf8);
-     // console.log("Decrypted: ", decrypted);
-     //
-     // console.log("Encrypted client_id: ", keys.client_id, " salt: ", salt, " iv: ", iv);
-     // const client_id_decrypted = CryptoJS.AES.decrypt(keys.client_id, salt.toString()).toString(CryptoJS.enc.Utf8);
-     // console.log("Decrypted: ", client_id_decrypted);
+    const fetchSalt = await fetch('https://sellomatr.herokuapp.com/salt', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({})
+    })
 
-     console.log(encryptedKeys);
+    const ek = await fetchKeys.text();
+    const iv = await fetchIv.text();
+    const salt = await fetchSalt.text();
+    const encryptedKeys = JSON.parse(ek);
 
-     const plaintextArray = await CryptoJS.AES.decrypt(
-         {ciphertext:  encryptedKeys},
-         salt,
-         {iv: iv}
-     );
+    const ebayAuthToken = await new EbayAuthToken({
+        clientId: decrypt(encryptedKeys.client_id, iv, salt),
+        clientSecret: decrypt(encryptedKeys.client_secret, iv, salt),
+        redirectUri: decrypt(encryptedKeys.redirect_uri, iv, salt),
+    });
 
-     console.log("Raw decrypted: ", plaintextArray);
-     console.log("To String: ", plaintextArray.toString());
-     // console.log(plaintextArray.toString(CryptoJS.enc.Utf8));
-     //
-     // const dcBase64String = plaintextArray.toString(CryptoJS.enc.Base64);
-     // console.log("Decrypted: ", dcBase64String);
-     // console.log("Decrypted: ", plaintextArray);
-     // // plain text array to string
-     // // const plaintext = plaintextArray.toString(CryptoJS.enc.Utf8);
-     // // console.log("Decrypted: ", {plaintext});
-     // // const client_id = JSON.parse(plaintext);
-     // console.log("Decrypted: ", plaintextArray.toString());
+    // Client scopes
+    const scopes = [
+        'https://api.ebay.com/oauth/api_scope/sell.fulfillment'
+    ];
 
+    if (refreshToken) {
+        // Generate access token from refresh token
+        const accessToken = await ebayAuthToken.getAccessToken('PRODUCTION', refreshToken, scopes);
 
+        await setStorageItem('accessToken', accessToken);
+    } else {
+        // Generate authorization URL to redirect the user
+        const options = {state: 'sellomatr-state', prompt: 'login'};
+        const authUrl = await ebayAuthToken.generateUserAuthorizationUrl('PRODUCTION', scopes, options);
 
+        await redirectToLogin(authUrl, ebayAuthToken);
+    }
+}
 
-
-
-
-
-     // const AESDecrypted = await CryptoJS.algo.AES.createDecryptor(salt, {
-     //     iv: iv,
-     // });
-     //
-     // console.log("IV: ", iv, "\nSalt: ", salt, "\nKeys: ", keys);
-     //
-     // const decryptedKeys = await AESDecrypted.finalize(keys);
-     // console.log("Decrypted Keys: ", decryptedKeys);
-
-     // const ebayAuthToken = await new EbayAuthToken({
-     //     clientId: AESDecrypted.process(keys.client_id),
-     //     clientSecret: AESDecrypted.process(keys.client_secret),
-     //     redirectUri: AESDecrypted.process(keys.redirect_uri),
-     // });
-
-     const ebayAuthToken = await new EbayAuthToken({
-         clientId: 'NathanLu-Sellomat-PRD-16ae2d65c-fa62dee0',
-         clientSecret: 'PRD-6ae2d65c31c2-c915-46d1-ab4f-c3d7',
-         redirectUri: 'Nathan_Lu-NathanLu-Sellom-wjsurbu'
-     });
-
-     // Client scopes
-     const scopes = [
-         'https://api.ebay.com/oauth/api_scope/sell.fulfillment'
-     ];
-
-     if (refreshToken) {
-         // Generate access token from refresh token
-         const accessToken = await ebayAuthToken.getAccessToken('PRODUCTION', refreshToken, scopes);
-         
-         await setStorageItem('accessToken', accessToken);
-     } else {
-         // Generate authorization URL to redirect the user
-         const options = {state: 'sellomatr-state', prompt: 'login'};
-         const authUrl = await ebayAuthToken.generateUserAuthorizationUrl('PRODUCTION', scopes, options);
-
-         await redirectToLogin(authUrl, ebayAuthToken);
-     }
- }
- 
 async function redirectToLogin(authUrl: string, ebayAuthToken: any) {
     // Create a new window with url
     await chrome.tabs.create({url: authUrl}).then(() => {
         chrome.tabs.onUpdated.addListener(createTokens);
+
         async function createTokens(tabId: number, changeInfo: chrome.tabs.TabChangeInfo, tab: chrome.tabs.Tab) {
             if (tabId === tab.id && changeInfo.status === 'complete') {
                 // Get code from oauth redirect
